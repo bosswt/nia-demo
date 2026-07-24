94f091f10c2f22e331044dee0910600b38cb8440
bosswt <boss.wt13@gmail.com>
2026-07-24 11:32:52 +0700

    chore: add openai

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 import { cors } from '@elysiajs/cors'
+import OpenAI from "openai";
+const client = new OpenAI();
 
-const app = new Elysia().use(cors()).get("/", () => {
+const app = new Elysia().use(cors()).get("/", async () => {
     return {
         message: "Hello World from NIA"
     }
